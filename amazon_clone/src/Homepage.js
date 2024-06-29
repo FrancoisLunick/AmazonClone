659dd4976290d9d1bd526201dc48e3e689ed3d6c
Lunick Francois <LunickFrancois22@gmail.com>
2024-06-29 14:50:48 -0400

    Update Homepage component to include NavigationBar, FeaturedProducts, and Categories
    - Updated the Homepage component to include the NavigationBar, FeaturedProducts, and Categories components.
    - Applied basic CSS styles to the Homepage component.
    - Ensured the layout is organized with a header, main content, and sections for featured products and categories.

--- amazon_clone/src/Homepage.js
+++ amazon_clone/src/Homepage.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import NavigationBar from './components/NavigationBar';
+import FeaturedProducts from './components/FeaturedProducts';
+import './styles/Homepage.css';
+import Categories from './components/Categories';
 
+/**
+ * Renders the Homepage component.
+ * 
+ * @returns {JSX.Element} The Homepage component 
+ */
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <div className='homepage'>Homepage
+      <NavigationBar/>
+      <header>
+        <h1>Amazon</h1>
+      </header>
+      <main>
+        <FeaturedProducts/>
+        <Categories/>
+      </main>
+    </div>
   )
 }
